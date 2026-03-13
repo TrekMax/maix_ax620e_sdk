276e63f3b519a0c4df72ab49f2e21844f8e3b93f
916BGAI <zhangjian@sipeed.com>
2026-03-13 15:41:59 +0800

    [ax620q nand] support auto get cmm size from boot

--- build/projects/AX620Q_nand_arm32_k419_sipeed_nanokvm/soc/scripts/auto_load_all_drv.sh
+++ build/projects/AX620Q_nand_arm32_k419_sipeed_nanokvm/soc/scripts/auto_load_all_drv.sh
@@ -6,11 +6,61 @@ else
     mode=$1
 fi
 
+if [ -f /boot/configs ]; then
+    . /boot/configs
+fi
+
+OS_MEM_MIN_SZIE=0
+
+function get_board_id()
+{
+    echo 0
+}
+
+function get_emmc_size()
+{
+    echo 256
+}
+
+function get_os_mem_size()
+{
+    $(cat /proc/cmdline | grep -o "mem=[0-9]*M" | sed 's/mem=\([0-9]*\)M/\1/')
+}
+
+function get_cmm_size()
+{
+    board_id=$(get_board_id)
+    emmc_size=$(get_emmc_size)
+    if [ -n "${maix_memory_cmm}" ] && [ ${maix_memory_cmm} -gt 0 ] && [ $((emmc_size - maix_memory_cmm)) -ge ${OS_MEM_MIN_SZIE} ]; then
+        echo ${maix_memory_cmm}
+    else
+        os_mem_size=$(get_os_mem_size)
+        echo $((emmc_size - os_mem_size))
+    fi
+}
+
+function get_cmm_param()
+{
+    emmc_size=$(get_emmc_size)
+    cmm_size=$(get_cmm_size)
+    os_mem_size=$OS_MEM_MIN_SIZE
+    if [ $((emmc_size - cmm_size)) -ge $OS_MEM_MIN_SZIE ]; then
+        os_mem_size=$((emmc_size - cmm_size))
+    else
+        os_mem_size=$((emmc_size / 2))
+        cmm_size=$((emmc_size / 2))
+    fi
+    offset=$((os_mem_size * 1024 * 1024 + 0x40000000))
+    printf "cmmpool=anonymous,0,%#x,%dM" "$offset" "$cmm_size"
+}
+
 function load_drv()
 {
     echo "run auto_load_all_drv.sh start "
     insmod /soc/ko/ax_sys.ko
-    insmod /soc/ko/ax_cmm.ko cmmpool=anonymous,0,0x80000000,2047M
+    cmm_param=$(get_cmm_param)
+    echo "insmod ax_cmm, param: $cmm_param"
+    insmod /soc/ko/ax_cmm.ko $cmm_param
     insmod /soc/ko/ax_pool.ko
     insmod /soc/ko/ax_base.ko
     insmod /soc/ko/ax_npu.ko
